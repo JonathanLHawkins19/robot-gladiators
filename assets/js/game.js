6fb58db24dfdf279a91c9abfb80ac8f0a8b1c431
JonathanLHawkins19 <hawkins.jonathan19@gmail.com>
2021-12-15 13:58:58 -0500

    game code

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,54 @@
 window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+// What is this?
+console.log(playerName);
+var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+function fight() {
+    window.alert("The fight has begun!");
+  }
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+var playerName = "Tony the Robot";
+
+// Tony the Robot is ready for battle!
+console.log("Tony the Robot" + " is ready for battle!");
+
+// "Tony the Robot is ready for battle!"
+console.log(playerName + " is ready for battle!");
+
+// "Your robot, Tony the Robot, has won!
+console.log("Your robot, " + playerName + ", has won!");
+var name = "your name";
+console.log(name);
+
+  // put new code under this
+console.log(
+    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+  );
+  
+  // check enemy's health
+  if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+  } 
+  else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+  }
+  fight();
+  // fight();
+  var playerHealth = 100;
+
+// check to see if the value of the playerHealth variable is greater than 0
+if (playerHealth > 0) {
+  console.log("Your player is still alive!");
+}
+var playerHealth = 10;
+
+if (playerHealth === 0) {
+  console.log("This will not run.");
+} 
+else {
+  console.log("This will run instead.");
+}
